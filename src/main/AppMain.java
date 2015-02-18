004e214fdfdcafa9e5fc5ddab6bf5b2d1acc77ff
Vegard Fjeldberg <vegard.fjeldberg@lyse.net>
2015-02-18 12:51:05 +0100

    Skrev ny linje

--- src/main/AppMain.java
+++ src/main/AppMain.java
@@ -3,6 +3,6 @@ package main;
 public class AppMain {
 	public static void main(String args[]) {
 		System.out.println("Dette skal vises på universitetsPC.");
-		
+		System.out.println("Her er en ny linje.");
 	}
 }
